076be0bb4c92bfbd1d8b8fe388282cb7fe4b948c
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-08-30 04:30:23 +0800

    copying and malloc

--- week4/code.c
+++ week4/code.c
@@ -1,18 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(void)
 {
-    char *s = get_string("s: ");
-    char *t = get_string("t: ");
+    string s = get_string("s: ");
 
-    if (strcmp(s, t) == 0)
-    {
-        printf("Same\n");
-    }
-    else
-    {
-        printf("Different\n");
-    }
+    string t = s;
+
+    t[0] = toupper(t[0]);
+
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
 }
