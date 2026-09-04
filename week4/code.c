cc4b65b4c42bbe8fbf857fd7cfac41981b4ab733
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-09-05 04:19:30 +0800

    copying using malloc

--- week4/code.c
+++ week4/code.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
@@ -7,9 +8,14 @@ int main(void)
 {
     char *s = get_string("s: ");
 
-    char *t = s;
+    char *t = malloc(strlen(s) + 1);
 
-    t[0] = toupper(*t);
+    for (int i = 0, n = strlen(s); i <= n; i++)
+    {
+        t[i] = s[i];
+    }
+
+    t[0] = toupper(t[0]);
 
     printf("s: %s\n", s);
     printf("t: %s\n", t);
